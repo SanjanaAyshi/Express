4c204817cba479c17695b9f0fbe2bf8963da2086
Sanjana Ayshi <sanjana.ayshi92@gmail.com>
2025-07-23 22:39:08 +0600

    console thing

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 import express, { json } from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+const morganFormat = ":method :url :status :response-time ms";
 
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 // app.get("/", (req, res) => {
 //   res.send("hello from earth");
 // });
